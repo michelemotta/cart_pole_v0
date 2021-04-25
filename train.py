652de624c8713b21b67476482406e8caaf0e1395
Michele Motta <ing_motta@bitbucket.org>
2021-04-25 19:42:11 +0200

    Project structure updated and successful solved the cart-pole game
    The file train.py is a very first tentative to implement the first visit Monte Carlo algorithm
    for solving the problem of the cart pole offered from: https://gym.openai.com/.
    
    From https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
    the problem is considered solved when the average return is greater than or equal to
    195.0 over 100 consecutive trials.
    
    In order to keep up exploration during the training a random action is selected.
    The algorithm starts to become greedy when the episodes are close to the pre-selected EPISODES.
    
    A score of 196.6 was obtained after 1200 episodes.

--- train.py
+++ train.py
@@ -2,17 +2,26 @@ import gym
 import time
 import math
 import random
+import pickle
+
+INFO_SCALER_STEP = 100
+EPISODES = 200 #193.0 averaged on 50 samples after 2000 episodes
 
 
 def getState(obs):
+    #TODO: use only position and angle in order to reduce state
+    #TODO: multiply the angle because is always -0.4<teta<0.4
     state = ''
-    for o in obs:
-        state += str(math.floor(o))
+    
+    state += str(math.floor(obs[0]))
+    state += str(math.floor(obs[1]))
+    state += str(math.floor(obs[2]*10.0))
+    state += str(math.floor(obs[3]))
     
     return state
 
 
-def policy(env, Q, state):
+def policy(env, Q, state, episode):
     #Find the maximum reward for a given state and see the action
     #Filter for current state and take the action with maximum reward
     current_state_knowledge = list(filter(lambda x: (x['state']==state), Q))
@@ -23,8 +32,10 @@ def policy(env, Q, state):
     greedy_action = max(current_state_knowledge, key=lambda x:x['average'])['action']
     #Be greedy only epsilon of the time
 
+    epsilon = 0.9 + episode/EPISODES
+
     random_value = random.random() # random number in range [0.0,1.0)
-    if random_value < 0.985:
+    if random_value < epsilon:
         return greedy_action
 
     return env.action_space.sample()
@@ -33,21 +44,32 @@ def policy(env, Q, state):
 def main():
     env = gym.make('CartPole-v0')
 
-    total_rewards = 0
     Q_s_a = []  #{state:__,action:__,tot:__, count:__,average:___}
     Returns_s_a = []
 
-    for i_episode in range(1000):
+    list_rewards = []
+
+    try:
+        with open('./obj/previous_knowledge.pickle', 'rb') as p:
+            Q_s_a = pickle.load(p)
+    except:
+        Q_s_a = []
+        print("Older sessions not found.")
+
+    cont_consecutive_success = 0
+
+    for i_episode in range(EPISODES):
         observation = env.reset()
 
         episode = []
         total_rewards = 0
+
         print('Episode: ', i_episode)
 
         for t in range(200):
             env.render()
             state = getState(observation)  # Get the state
-            action = policy(env, Q_s_a, state)  # Get the action
+            action = policy(env, Q_s_a, state, i_episode)  # Get the action
 
             #print(observation)
 
@@ -58,7 +80,7 @@ def main():
             episode.append({'state': state, 'action': action, 'reward': reward})
 
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                #print("Episode finished after {} timesteps".format(t+1))
 
                 episode = find_occurrences(episode)
                 #print(episode)
@@ -81,10 +103,27 @@ def main():
                 #print(Q_s_a)
 
                 print("Total rewards for this episode is: ", total_rewards)
+                list_rewards.append(total_rewards)
+
+                if total_rewards >= 195.0:
+                    cont_consecutive_success += 1
+                    print("Consecutive successfull episodes: ", cont_consecutive_success)
+                else:
+                    cont_consecutive_success = 0
 
                 break
 
+        if ((i_episode+1)%INFO_SCALER_STEP) == 0:
+            print("Averaged rewards: ", sum(list_rewards)/INFO_SCALER_STEP)
+            list_rewards.clear()
+
     env.close()
+
+    #try:
+    with open('./obj/previous_knowledge.pickle', 'wb') as p:
+        pickle.dump(Q_s_a, p)
+    #except:
+     #   print("Error in saving the knowledge!")
 
 def find_occurrences(episode):
     for _, step in enumerate(episode):
