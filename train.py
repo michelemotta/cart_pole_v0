7ea8c7472352a6d83e653d8005ca8ef00c5d8e8b
Michele Motta <ing_motta@bitbucket.org>
2021-04-25 21:14:07 +0200

    Cleaning some comments and improving code readibility

--- train.py
+++ train.py
@@ -1,40 +1,64 @@
+"""
+On-policy first-visit MC control
+
+Details about the problem:
+https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+"""
+
 import gym
 import time
 import math
 import random
 import pickle
 
-INFO_SCALER_STEP = 100
-EPISODES = 200 #193.0 averaged on 50 samples after 2000 episodes
-
+#Number of episodes used for the statistics calculation
+INFO_SCALER = 100
+#Number of episodes used for the training
+EPISODES = 200
 
 def getState(obs):
-    #TODO: use only position and angle in order to reduce state
-    #TODO: multiply the angle because is always -0.4<teta<0.4
+    """
+    Observation:
+        Type: Box(4)
+        Num     Observation               Min                     Max
+        0       Cart Position             -4.8                    4.8
+        1       Cart Velocity             -Inf                    Inf
+        2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
+        3       Pole Angular Velocity     -Inf                    Inf
+    """
     state = ''
-    
+
+    #TODO: experiment with various resolution for each state
+    #Cart position (only integer)
     state += str(math.floor(obs[0]))
+    #Cart velocity
     state += str(math.floor(obs[1]))
+    #Pole angle
     state += str(math.floor(obs[2]*10.0))
+    #Pole angular velocity
     state += str(math.floor(obs[3]))
-    
+
     return state
 
 
 def policy(env, Q, state, episode):
-    #Find the maximum reward for a given state and see the action
-    #Filter for current state and take the action with maximum reward
+    """
+    Find the maximum reward for a given state and find the greedy action.
+    The greedy action is selected only 1-epsilon of the time.
+    """
     current_state_knowledge = list(filter(lambda x: (x['state']==state), Q))
 
+    #The state is not yet present in the policy
     if not current_state_knowledge:
         return env.action_space.sample()
-    
-    greedy_action = max(current_state_knowledge, key=lambda x:x['average'])['action']
-    #Be greedy only epsilon of the time
 
+    greedy_action = max(current_state_knowledge, key=lambda x:x['average'])['action']
+
+    #Be greedy only epsilon of the time
     epsilon = 0.9 + episode/EPISODES
 
-    random_value = random.random() # random number in range [0.0,1.0)
+    #Random number in range [0.0,1.0)
+    random_value = random.random()
     if random_value < epsilon:
         return greedy_action
 
@@ -44,19 +68,20 @@ def policy(env, Q, state, episode):
 def main():
     env = gym.make('CartPole-v0')
 
-    Q_s_a = []  #{state:__,action:__,tot:__, count:__,average:___}
-    Returns_s_a = []
+    Q_s_a = [] #{state:__,action:__,tot:__, count:__,average:___}
 
     list_rewards = []
 
+    #Only for statistics: used to count the consecutive successfully episodes
+    cont_consecutive_success = 0
+
+    #Try to restore the older knowledge
     try:
         with open('./obj/previous_knowledge.pickle', 'rb') as p:
             Q_s_a = pickle.load(p)
     except:
         Q_s_a = []
-        print("Older sessions not found.")
-
-    cont_consecutive_success = 0
+        print("Old knowledge not found.")
 
     for i_episode in range(EPISODES):
         observation = env.reset()
@@ -64,29 +89,29 @@ def main():
         episode = []
         total_rewards = 0
 
-        print('Episode: ', i_episode)
-
         for t in range(200):
             env.render()
-            state = getState(observation)  # Get the state
-            action = policy(env, Q_s_a, state, i_episode)  # Get the action
 
-            #print(observation)
+            # Get the state
+            state = getState(observation)
+            # Get the action
+            action = policy(env, Q_s_a, state, i_episode)
 
             observation, reward, done, info = env.step(action)
 
-            total_rewards += reward # Update total reward for this episode
+            total_rewards += reward
 
             episode.append({'state': state, 'action': action, 'reward': reward})
 
+            #Episode finished: update the state-action value function
             if done:
-                #print("Episode finished after {} timesteps".format(t+1))
 
+                #Enumerate all the times a state is visited
                 episode = find_occurrences(episode)
-                #print(episode)
 
                 G = 0
                 gamma = 0.95
+                #Travel through steps in reverse order
                 for cont, step in enumerate(reversed(episode)):
 
                     G = (gamma*G) + step['reward']
@@ -96,15 +121,16 @@ def main():
                             if (q['state'] == step['state']) and (q['action'] == step['action']):
                                 q['tot'] += G
                                 q['count'] += 1
-                                q['average'] = q['tot']/q['count'] 
+                                q['average'] = q['tot']/q['count']
                                 continue
-                        #New state action couple
-                        Q_s_a.append({'state': step['state'], 'action': step['action'], 'tot': G, 'count': 1, 'average': G})
-                #print(Q_s_a)
 
-                print("Total rewards for this episode is: ", total_rewards)
+                        #New state action: append it to the value function
+                        Q_s_a.append({'state': step['state'], 'action': step['action'], 'tot': G, 'count': 1, 'average': G})
+
+                print("Total rewards for episode {} is: ".format(i_episode), total_rewards)
                 list_rewards.append(total_rewards)
 
+                #Update the statistics
                 if total_rewards >= 195.0:
                     cont_consecutive_success += 1
                     print("Consecutive successfull episodes: ", cont_consecutive_success)
@@ -113,22 +139,25 @@ def main():
 
                 break
 
-        if ((i_episode+1)%INFO_SCALER_STEP) == 0:
-            print("Averaged rewards: ", sum(list_rewards)/INFO_SCALER_STEP)
+        #After INFO_SCALER episodes calculate the averaged rewards
+        if ((i_episode+1)%INFO_SCALER) == 0:
+            print("Averaged rewards: ", sum(list_rewards)/INFO_SCALER)
             list_rewards.clear()
 
     env.close()
 
-    #try:
-    with open('./obj/previous_knowledge.pickle', 'wb') as p:
-        pickle.dump(Q_s_a, p)
-    #except:
-     #   print("Error in saving the knowledge!")
+    try:
+        with open('./obj/previous_knowledge.pickle', 'wb') as p:
+            pickle.dump(Q_s_a, p)
+    except:
+        print("Error in saving the knowledge!")
 
+#Find the number of times a state is visited within a single episode and report it
 def find_occurrences(episode):
     for _, step in enumerate(episode):
         occurrences = 0
 
+        #The state has been already counted in one or more previous iterations
         if 'i' in step:
             continue
 
